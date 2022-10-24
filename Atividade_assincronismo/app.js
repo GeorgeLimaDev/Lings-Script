e978726148a8ca199508515caffb3d4105115bb4
georgelacademico <george.lima@academico.ifpb.edu.br>
2022-10-24 19:23:35 -0300

    Melhorando tratamento de erros.

--- Atividade_assincronismo/app.js
+++ Atividade_assincronismo/app.js
@@ -3,38 +3,43 @@ let cep = document.getElementById('cep')
 cep.addEventListener('focusout', lerCEP)
 
 //Função para passar o CEP digitado para a API:
-function lerCEP () {
-    let cepValue = cep.value
-    let cepLido = `https://viacep.com.br/ws/${cepValue}/json/`
-    getCEP(cepLido)
+function lerCEP() {
+  let cepValue = cep.value
+  let cepLido = `https://viacep.com.br/ws/${cepValue}/json/`
+  getCEP(cepLido)
 }
 
 //Função assíncrona que recebe o CPF e traz o endereço:
 async function getCEP(cepLido) {
-    try {
-        const response = await fetch(cepLido)
-        const json = await response.json()
-        showContent(json)
-        cep.style.border = '' //Esta linha é pra voltar ao padrão depois de uma tentativa incorreta, se não fica sempre vermelho.
-    } catch {
-        erroCEP()
+  try {
+    const response = await fetch(cepLido)
+    const json = await response.json()
+    showContent(json)
+    cep.style.border = '' //Esta linha é pra voltar ao padrão depois de uma tentativa incorreta, se não fica sempre vermelho.
+    if (json.uf == undefined) {
+      erroCEP()
     }
+  } catch {
+    erroCEP()
+  }
 }
 
 //Função que exibe os dados recebidos na página:
 function showContent(json) {
-    let logradouro = document.getElementById('logradouro')
-    logradouro.value = json.logradouro
-    let bairro = document.getElementById('bairro')
-    bairro.value = json.bairro
-    let cidade = document.getElementById('cidade')
-    cidade.value = json.localidade
-    let estado = document.getElementById('estado')
-    estado.value = json.uf
+  let logradouro = document.getElementById('logradouro')
+  logradouro.value = json.logradouro
+  let bairro = document.getElementById('bairro')
+  bairro.value = json.bairro
+  let cidade = document.getElementById('cidade')
+  cidade.value = json.localidade
+  let estado = document.getElementById('estado')
+  estado.value = json.uf
 }
 
 //Função executada quando o tamanho do CEP digitado for diferente de 8 dígitos:
 function erroCEP() {
-    window.alert('Digite um CEP válido.')
-    cep.style.border = '1px solid red'
+  cep.style.border = '1px solid red'
+  let form = document.getElementById('form')
+  form.reset()
+  cep.placeholder = 'CEP inválido ou incorreto. Tente novamente.'
 }
